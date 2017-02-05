4b572de9e1adc20d3bf2bc660172b71623320ad8
Michihide Hotta <hotta@net-newbie.com>
2017-02-05 16:08:22 +0900

    refactoring for wordpress

--- roles/wordpress/templates/wp-config.php
+++ roles/wordpress/templates/wp-config.php
@@ -81,7 +81,7 @@ $table_prefix  = 'wp_';
  *
  * @link http://wpdocs.osdn.jp/WordPress%E3%81%A7%E3%81%AE%E3%83%87%E3%83%90%E3%83%83%E3%82%B0
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* 編集が必要なのはここまでです ! WordPress でブログをお楽しみください。 */
 
